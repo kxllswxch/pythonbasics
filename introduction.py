ddc6b6c9fb42ba8675c37c4841629efbf7828840
kxllswxch <kxlswxch@gmail.com>
2026-02-03 10:27:24 +0300

    first python

---
+++ introduction.py
@@ -0,0 +1,4 @@
+print("welcome to python programming")
+print("Jeff")
+print(100)
+print(20*3)
---
+++ variables.py
@@ -0,0 +1,15 @@
+#variable is a container used to store a value
+school = "eMobilis"
+firstname = "Jeff"
+lastname = "Alexander"
+num1 = 36
+num2 = 42
+language = "python"
+
+print(school)
+print(firstname, lastname)
+print(num1+num2)
+print(language, "is a web programming language")
+
+
+#the sum of 36 and 42 is 78
